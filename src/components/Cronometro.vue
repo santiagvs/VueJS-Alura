a56a3b4b57ef6fe541309df88dacaf9c217702b5
Santiago <victor.lacsilva0102@gmail.com>
2023-04-18 15:56:48 -0300

    refactor: dividing Form cronometer to a new file

---
+++ src/components/Cronometro.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <strong>
+      {{ tempoDecorrido }}
+    </strong>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CronometroApp",
+  props: {
+    tempoEmSegundos: {
+      type: Number,
+      default: 0,
+    },
+  },
+  computed: {
+    tempoDecorrido() {
+      return new Date(this.tempoEmSegundos * 1000)
+        .toISOString()
+        .substring(19, 11);
+    },
+  },
+});
+</script>
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -16,11 +16,7 @@
         <div
           class="is-flex is-align-items-center is-justify-content-space-between"
         >
-          <section>
-            <strong>
-              {{ tempoDecorrido }}
-            </strong>
-          </section>
+          <Cronometro :tempoEmSegundos="tempoEmSegundos" />
           <button class="button" @click="iniciar">
             <span class="icon">
               <i class="fas fa-play"></i>
@@ -41,21 +37,18 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Cronometro from "./Cronometro.vue";
 
 export default defineComponent({
   name: "FormularioApp",
+  components: {
+    Cronometro,
+  },
   data() {
     return {
       tempoEmSegundos: 0,
       cronometro: 0,
     };
-  },
-  computed: {
-    tempoDecorrido() {
-      return new Date(this.tempoEmSegundos * 1000)
-        .toISOString()
-        .substring(19, 11);
-    },
   },
   methods: {
     iniciar() {
