d352fb9b73b7c8bed4dfa5c8acd1b4155254c3bb
Santiago <victor.lacsilva0102@gmail.com>
2023-04-18 16:29:54 -0300

    feat/refactor: setting events and separating the Form button into a component

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -10,10 +10,11 @@
           type="text"
           class="input"
           placeholder="Qual tarefa você deseja iniciar?"
+          v-model="descricao"
         />
       </div>
       <div class="column">
-        <Temporizador />
+        <Temporizador @aoTemporizadorFinalizado="finalizarTarefa" />
       </div>
     </div>
   </div>
@@ -30,19 +31,14 @@ export default defineComponent({
   },
   data() {
     return {
-      tempoEmSegundos: 0,
-      cronometro: 0,
+      descricao: "",
     };
   },
   methods: {
-    iniciar() {
-      this.cronometro = setInterval(() => {
-        this.tempoEmSegundos++;
-      }, 1000);
-      console.log("iniciando");
-    },
-    finalizar() {
-      clearInterval(this.cronometro);
+    finalizarTarefa(tempoDecorrido: number): void {
+      console.log("tempo da tarefa", tempoDecorrido);
+      console.log("descrição da tarefa", this.descricao);
+      this.descricao = "";
     },
   },
 });
